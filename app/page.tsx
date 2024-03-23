e28d56a5a38c47632b31f5d4529cfcd4b988855c
Goraya2005 <goraya2005@gmail.com>
2024-03-23 06:26:23 +0500

    Add initial files

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
-import React from react
 
+'use client'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function Todo() {
-  return (
-    <div> <TODOS></TODOS></div>
+export default function Products(){
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+
+    getProducts()
+  },[])
+
+  const getProducts = async () => {
+    try {
+      const result = await axios.get('https://dummyjson.com/products?limit=10&skip=10&select=title,price');
+      console.log('List of Products fetched through axios from Dummy JSON', result.data.products);
+      setProducts(result.data.products);
+    } catch (error) {
+      console.log('API is not properly fetched', error);
+    }
+  }
+
+  return(
+    <div>
+      <h1>LIST OF PRODUCTS</h1>
+      <button onClick={getProducts}>Get Products</button>
+      {products.map((product) => (
+        <div key={product.id}>
+          {product.id} - {product.title} - {product.price}
+        </div>
+      ))}
+    </div>
   )
+}
